46af3ddeae851ccda021119bfc5697fff3e359b1
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-07-03 20:21:10 +0300

    Added possibility to specify index for OnThis attribute to be able to grab more than one of the same component.

--- Editor/CustomMonoBehaviourEditor.cs
+++ Editor/CustomMonoBehaviourEditor.cs
@@ -17,6 +17,9 @@ namespace ThreeDent.DevelopmentTools.Editor
     [CustomEditor(typeof(MonoBehaviour), true)]
     public class CustomMonoBehaviourEditor : UnityEditor.Editor
     {
+        private const string ComponentOnThisNotFoundMessage = "This script requires component \"{0}\" to be present on this object.";
+        private const string ComponentOnThisIndexNotFoundMessage = "This script requires component \"{0}\" to be present on this object under index {1}.";
+
         private GameObject gameObject;
 
         private static bool HasRequiredAttribute(MemberInfo x)
@@ -33,14 +36,18 @@ namespace ThreeDent.DevelopmentTools.Editor
 
         private void AddFieldsWithoutAttributes(VisualElement root)
         {
-            var fieldsWithoutAttribute = GetSerializableFields(target.GetType()).Where(x => !HasRequiredAttribute(x));
-            var properties = fieldsWithoutAttribute.Select(x => serializedObject.FindProperty(x.Name));
-            foreach (var property in properties)
-                root.Add(new PropertyField(property));
+            GetSerializableFields(target.GetType())
+            .Where(x => !HasRequiredAttribute(x))
+            .Select(x => serializedObject.FindProperty(x.Name))
+            .ForEach(x => root.Add(new PropertyField(x)));
         }
 
         private void FillParentsOnChildFields()
         {
+            // collect all the parents.
+            // collect all the attached MonoBehavior scripts.
+            // 
+
             // var parent = gameObject.transform.parent;
             // while (parent != null)
             // {
@@ -49,10 +56,7 @@ namespace ThreeDent.DevelopmentTools.Editor
             //         var serializedParent = new SerializedObject(parent);
             //         var onChildFields = GetSerializableFields(script.GetType()).Where(x => x.IsDefined<OnChildAttribute>());
             //         foreach (var field in onChildFields)
-
             //     }
-
-
             //     parent = parent.transform.parent;
             // }
             // var obj = new SerializedObject(gameObject.transform.parent.gameObject);
@@ -66,10 +70,19 @@ namespace ThreeDent.DevelopmentTools.Editor
             var property = serializedObject.FindProperty(field.Name);
             property.objectReferenceValue = null;
 
-            if (gameObject.TryGetComponent(field.FieldType, out var component))
-                property.objectReferenceValue = component;
+            var index = field.GetCustomAttribute<OnThisAttribute>().Index;
+            var components = gameObject.GetComponents(field.FieldType);
+            if (components.Length > index)
+            {
+                property.objectReferenceValue = components[index];
+            }
             else
-                root.Add(new HelpBox($@"This script requires component ""{field.FieldType.Name}"" to be present on this object.", HelpBoxMessageType.Warning));
+            {
+                if (index == 0)
+                    root.Add(new HelpBox(string.Format(ComponentOnThisNotFoundMessage, field.FieldType.Name), HelpBoxMessageType.Warning));
+                else
+                    root.Add(new HelpBox(string.Format(ComponentOnThisIndexNotFoundMessage, field.FieldType.Name, index), HelpBoxMessageType.Warning));
+            }
 
             serializedObject.ApplyModifiedProperties();
         }
@@ -103,12 +116,16 @@ namespace ThreeDent.DevelopmentTools.Editor
             }
         }
 
+        protected virtual void OnEnable()
+        {
+            gameObject = ((MonoBehaviour)target).gameObject;
+        }
+
         public override VisualElement CreateInspectorGUI()
         {
             if (CustomMonoBehaviourEditorUsageController.UseCustomMonoBehaviourEditor)
             {
                 var root = new VisualElement();
-                gameObject = ((MonoBehaviour)target).gameObject;
                 HandleCustomAttributes(root);
                 AddFieldsWithoutAttributes(root);
                 FillParentsOnChildFields();
--- OnThisAttribute.cs
+++ OnThisAttribute.cs
@@ -3,5 +3,20 @@ using System;
 namespace ThreeDent.DevelopmentTools
 {
     [AttributeUsage(AttributeTargets.Field)]
-    public class OnThisAttribute : Attribute { }
+    public class OnThisAttribute : Attribute
+    {
+        private int index;
+
+        public int Index => index;
+
+        public OnThisAttribute()
+        {
+            index = 0;
+        }
+
+        public OnThisAttribute(int index)
+        {
+            this.index = index;
+        }
+    }
 }
