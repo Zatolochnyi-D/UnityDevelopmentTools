0e7bde390403c25c347820ee1229e7e67f6856b2
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-07-03 20:43:21 +0300

    Added additional fields for OnChild attribute to be able to grab references to other same components in children.

--- OnChildAttribute.cs
+++ OnChildAttribute.cs
@@ -2,6 +2,31 @@ using System;
 
 namespace ThreeDent.DevelopmentTools
 {
+    public enum OnChildTraversingMode
+    {
+        BFS,
+        DFS,
+    }
+
     [AttributeUsage(AttributeTargets.Field)]
-    public class OnChildAttribute : Attribute { }
+    public class OnChildAttribute : Attribute
+    {
+        private int offset;
+        private OnChildTraversingMode traversingMode;
+
+        public int Index => offset;
+        public OnChildTraversingMode TraversingMode => traversingMode;
+
+        public OnChildAttribute(OnChildTraversingMode traversingMode = OnChildTraversingMode.BFS)
+        {
+            offset = 0;
+            this.traversingMode = traversingMode;
+        }
+
+        public OnChildAttribute(int index, OnChildTraversingMode traversingMode = OnChildTraversingMode.BFS)
+        {
+            this.offset = index;
+            this.traversingMode = traversingMode;
+        }
+    }
 }
