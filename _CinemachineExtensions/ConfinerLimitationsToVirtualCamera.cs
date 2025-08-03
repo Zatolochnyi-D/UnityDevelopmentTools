ed4f0d019cef0457e0787846b36f9c5ef05d75b2
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-08-03 10:03:00 +0300

    Moved extension to cinemachine namespace.

--- _CinemachineExtensions/ConfinerLimitationsToVirtualCamera.cs
+++ _CinemachineExtensions/ConfinerLimitationsToVirtualCamera.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 using Unity.Cinemachine;
 
-[SaveDuringPlay, AddComponentMenu("")]
-public class ConfinerLimitationsToVirtualCamera : CinemachineExtension
+namespace ThreeDent.DevelopmentTools.CinemachineExtensions
 {
-    protected override void PostPipelineStageCallback(
-        CinemachineVirtualCameraBase vcam,
-        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    [SaveDuringPlay, AddComponentMenu("")]
+    public class ConfinerLimitationsToVirtualCamera : CinemachineExtension
     {
-        if (stage == CinemachineCore.Stage.Finalize)
+        protected override void PostPipelineStageCallback(
+            CinemachineVirtualCameraBase vcam,
+            CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
         {
-            state.RawPosition = state.GetCorrectedPosition();
-            state.PositionCorrection = Vector3.zero;
-            state.RawOrientation = state.GetCorrectedOrientation();
-            state.OrientationCorrection = Quaternion.identity;
+            if (stage == CinemachineCore.Stage.Finalize)
+            {
+                state.RawPosition = state.GetCorrectedPosition();
+                state.PositionCorrection = Vector3.zero;
+                state.RawOrientation = state.GetCorrectedOrientation();
+                state.OrientationCorrection = Quaternion.identity;
+            }
         }
     }
 }
