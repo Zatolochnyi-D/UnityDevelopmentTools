04418fe8449478f14d1dd79038bad2248566978b
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-08-03 09:56:38 +0300

    Added extension class for Cinemachine with assembly that loaded only when Cinemachine package is present.

---
+++ _CinemachineExtensions/ConfinerLimitationsToVirtualCamera.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+[SaveDuringPlay, AddComponentMenu("")]
+public class ConfinerLimitationsToVirtualCamera : CinemachineExtension
+{
+    protected override void PostPipelineStageCallback(
+        CinemachineVirtualCameraBase vcam,
+        CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if (stage == CinemachineCore.Stage.Finalize)
+        {
+            state.RawPosition = state.GetCorrectedPosition();
+            state.PositionCorrection = Vector3.zero;
+            state.RawOrientation = state.GetCorrectedOrientation();
+            state.OrientationCorrection = Quaternion.identity;
+        }
+    }
+}
