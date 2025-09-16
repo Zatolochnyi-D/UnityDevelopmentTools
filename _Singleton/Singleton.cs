ee69b2652d677580625235c7ac792cd08bcefe64
ZatolochnyiD <d.zatolochnyi@gmail.com>
2025-09-16 13:36:21 +0300

    Removed even more comments.

--- _Singleton/Singleton.cs
+++ _Singleton/Singleton.cs
@@ -3,16 +3,11 @@ using UnityEngine;
 
 namespace ThreeDent.DevelopmentTools.Singleton
 {
-    /// <summary>
-    /// Class that handles singleton set up.
-    /// </summary>
-    /// <typeparam name="T">Class that inherits singleton.</typeparam>
     public class Singleton<T> : MonoBehaviour where T : Singleton<T>
     {
         private static T instance;
 
-        /// <exception cref="SingletonMissingException">Thrown when singleton was not created at the moment of calling this getter.</exception>
-        public static T Instance => instance ?? throw new SingletonMissingException($"Singleton {typeof(T)} does not exist currently. Check if it calls base.Awake() or at least exists on scene.");
+        public static T Instance => instance ?? throw new SingletonMissingException($"Singleton {typeof(T)} does not exist currently. Check if it calls base.Awake() or exists on scene.");
 
         protected virtual void Awake()
         {
