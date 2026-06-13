372270fb94ab2f86db3e785ce3f24a02517f7cf0
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-06-13 12:45:56 +0300

    Renamed base IM to PlainInputManager for IMs as plain classes used in GO, and created MB based base IM to extend using MonoBehaviour system.

---
+++ _Systems/_InputSystem/MonoBehaviourInputManager.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace DenZ.DevelopmentTools.InputSystem
+{
+    /// <summary>
+    /// Shorthand for custom Input Manager components (attached to GOs), that automatically creates instance of Input Actions,
+    /// provided in generic parameter.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class MonoBehaviourInputManager<T> : MonoBehaviour where T : IInputActionCollection2, IDisposable, new()
+    {
+        protected T InputActions;
+
+        protected virtual void Awake()
+        {
+            InputActions = new();
+        }
+    }
+}
---
+++ _Systems/_InputSystem/PlainInputManager.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine.InputSystem;
+
+namespace DenZ.DevelopmentTools.InputSystem
+{
+    /// <summary>
+    /// Shorthand for custom plain Input Managers (used in DI), that automatically creates instance of Input Actions, provided in
+    /// generic parameter.
+    /// </summary>
+    /// <typeparam name="T">Unity's Input Actions, that will be wrapped by this Input Manager.</typeparam>
+    public abstract class PlainInputManager<T> where T : IInputActionCollection2, IDisposable, new()
+    {
+        protected T InputActions;
+
+        public PlainInputManager()
+        {
+            InputActions = new();
+        }
+    }
+}
