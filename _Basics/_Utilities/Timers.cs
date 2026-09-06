a32e00cfd05e34d5c115ff9e0b5fac52025014f8
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-09-06 21:28:46 +0300

    Added new Timer, that calls method every x seconds indefinitely.

--- _Basics/_Utilities/Timers.cs
+++ _Basics/_Utilities/Timers.cs
@@ -6,12 +6,14 @@ namespace DenZ.DevelopmentTools.Utilities
 {
     public static class Timers
     {
-        public static async Awaitable InvokeEachFrameIndefinitely(Action actionToCall, CancellationToken token = default)
+        public static async Awaitable InvokeEachFrameIndefinitely(Action actionToCall, CancellationToken token = default, bool callOnStart = false)
         {
+            if (callOnStart)
+                actionToCall.Invoke();
             while (!token.IsCancellationRequested)
             {
-                actionToCall.Invoke();
                 await Awaitable.NextFrameAsync(token);
+                actionToCall.Invoke();
             }
         }
 
@@ -26,12 +28,21 @@ namespace DenZ.DevelopmentTools.Utilities
         public static async Awaitable InvokeOnce(Action actionToCall, int frames, CancellationToken token = default)
         {
             for (int i = 0; i < frames; i++)
-            {
                 await Awaitable.NextFrameAsync(token);
-                if (token.IsCancellationRequested)
-                    return;
-            }
+            if (token.IsCancellationRequested)
+                return;
             actionToCall.Invoke();
+        }
+
+        public static async Awaitable InvokeIndefinitely(Action actionToCall, float seconds, CancellationToken token = default, bool callOnStart = false)
+        {
+            if (callOnStart)
+                actionToCall.Invoke();
+            while (!token.IsCancellationRequested)
+            {
+                await Awaitable.WaitForSecondsAsync(seconds, token);
+                actionToCall.Invoke();
+            }
         }
     }
 }
